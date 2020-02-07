a15ded5d9181b4964c6e4b1127e07f4b64ad5741
Rodion Chachura <geekrodion@gmail.com>
2020-02-07 07:27:01 +0300

    update: deployment

--- management/deploy.sh
+++ management/deploy.sh
@@ -1,4 +1,4 @@
-npm run build
-aws s3 cp build s3://$BUCKET --recursive
+gatsby build
+aws s3 cp public s3://$BUCKET --recursive
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
