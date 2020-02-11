e8ef3fbca833bb1da4d641e74a58bf9c3c7c2945
Rodion Chachura <geekrodion@gmail.com>
2020-02-11 15:38:34 +0300

    update: no header for  index

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
   if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
+    header = null
   } else {
     header = (
       <h3
